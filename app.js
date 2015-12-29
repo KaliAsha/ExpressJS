4138a2e19e7c0d2168578be73f469c6dcdc32386
KaliAsha <kaliasha.kali@gmail.com>
2015-12-29 18:19:11 +0100

    Add Socket.io

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var socketio = require('socket.io');
 
 var app = express();
+
+// Socket.io setup
+var io = socketio();
+app.io = io;
+
+var routes = require('./routes/index')(io);
+var users = require('./routes/users')(io);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
